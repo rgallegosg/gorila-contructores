2c7d57ac4fa699bcac2ba44800a37e7e02ef9c12
Ricardo <gallegos.rickgalle@gmail.com>
2025-01-22 09:46:28 -0600

    GC-0008

--- gorilla-constructores/src/routes/services/services.component.jsx
+++ gorilla-constructores/src/routes/services/services.component.jsx
@@ -1,13 +1,15 @@
 import { Fragment } from "react";
+import ServiceCard from "../../components/services/services/service-card.component";
 
 const ServicesPage = () => {
     return (
         <Fragment>
             <div className="services-container">
-                <h2 className="h2-services">Servicios</h2>
+                <h2 className="h2-services">Nuestros Servicios</h2>
+                <ServiceCard></ServiceCard>
             </div>
         </Fragment>
     );
 };
 
-export default ServicesPage;
+export default ServicesPage; 
